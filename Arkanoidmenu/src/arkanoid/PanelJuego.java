7876c051734da8a4ed48f1872b2976e8de61c613
Jubiito1 <bollingerjulianguido@gmail.com>
2025-08-19 18:00:16 -0300

    Add files via upload

---
+++ Arkanoidmenu/src/arkanoid/PanelJuego.java
@@ -0,0 +1,12 @@
+package arkanoid;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelJuego extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+    public PanelJuego() {
+        setBackground(Color.BLACK); // por ahora negro, después dibujamos la bola, paleta, etc.
+    }
+}
